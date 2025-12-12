85564048107880debba766cabde1200719c52bad
balambigai01 <balambigai6050@gmail.com>
2025-12-12 12:54:02 +0530

    removed db ocnnection in app.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,5 @@
 import app from "./src/app.js";
-import { connectDB } from "./src/config/db.js";
 
-
-connectDB();
 
 const PORT = 8000;
 app.listen(PORT, () => console.log("Backend running on the port",PORT ))
